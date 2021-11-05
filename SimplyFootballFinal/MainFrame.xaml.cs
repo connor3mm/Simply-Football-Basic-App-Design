d050685d4aae543ff60dc90f4fe391cdd9ea86c2
Connor <connor.mcguire.2020@uni.strath.ac.uk>
2021-11-05 23:40:18 +0000

    Add project files.

---
+++ SimplyFootballFinal/MainFrame.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimplyFootballFinal
+{
+    /// <summary>
+    /// Interaction logic for MainFrame.xaml
+    /// </summary>
+    public partial class MainFrame : Window
+    {
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAddFootballer_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerMember Awindow = new PlayerMember();
+            Awindow.ShowDialog();
+        }
+
+        private void btnFootProfile_Click(object sender, RoutedEventArgs e)
+        {
+            playerProfile Awindow = new playerProfile();
+            Awindow.ShowDialog();
+        }
+
+        private void BtnListAll_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ SimplyFootballFinal/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimplyFootballFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame Awindow = new MainFrame();
+            Awindow.ShowDialog();
+            //string textBoxPass;
+            //string textBoxUser;
+            //textBoxPass = passwordBox.Text;
+            //textBoxUser = txtBoxUser.Text;
+            //validUser(textBoxUser);
+            //validPass(textBoxPass);
+        }
+
+        /// <summary>
+        /// Regex for password
+        /// </summary>
+        /// <param name="pass">password</param>
+        /// <returns>Password valid or not</returns>
+        public bool validPass(string pass)
+        {
+            var input = pass;
+
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Please enter Password");
+            }
+
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,15}");
+
+            var valid = hasNumber.IsMatch(input) && hasUpperChar.IsMatch(input) && hasMiniMaxChars.IsMatch(input);
+
+            return valid;
+        }
+
+
+        /// <summary>
+        /// To validate the username
+        /// </summary>
+        /// <param name="user">username</param>
+        /// <returns>username valid or not</returns>
+        public bool validUser(string user)
+        {
+            var input = user;
+
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Please enter Username");
+            }
+
+            var startsWith = new Regex(@"[A-Za-z0-9]");
+            var hasMiniMaxChars = new Regex(@"[A-Za-z0-9._()\[\]-]{3,15}");
+
+            var valid = startsWith.IsMatch(input) && hasMiniMaxChars.IsMatch(input);
+
+            return valid;
+        }
+    }
+}
